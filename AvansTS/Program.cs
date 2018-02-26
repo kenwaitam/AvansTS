29a8c0cf9ef2663f765079b599941f8c10e96e6b
Richard Bos <richard.bos93@gmail.com>
2018-02-26 14:35:09 +0100

    Added Main Program

---
+++ AvansTS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AvansTS.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello World!");
+        }
+    }
+}
